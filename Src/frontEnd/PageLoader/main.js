a5fd4a46f05a399779a4cda89732635ca39472aa
pendragons-code <pendragonscode@gmail.com>
2023-04-26 02:25:34 +0800

    variable

--- Src/frontEnd/PageLoader/main.js
+++ Src/frontEnd/PageLoader/main.js
@@ -6,8 +6,8 @@ module.exports = {
 		let videosDirs = readdirSync("./Src/frontEnd/public/Videos").filter(dirs => dirs)
 		for(dirs of videosDirs) {
 			let videoFile = readdirSync(`./Src/frontEnd/public/Videos/${dirs}`).filter(file => file)
-			for(file of videoFile) {
-				links += `<a href="/Videos/${dirs}/${videoFile}">${videoFile}</a>`
+			for(file of videoFile) {				
+				links += `<a href="/Videos/${dirs}/${file}">${file}</a><br>`
 			}
 		}
 		return res.render("main.ejs", { links: links })
